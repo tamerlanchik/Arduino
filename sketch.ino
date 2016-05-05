0204ef63a5e82eb27b92bf09bd1b9d88eb1ae2b3
Andrey K <andreykochnov@yandex.ru>
2016-05-05 22:25:58 +0300

    Перешито с расчетом на прием двух байтов

--- sketch.ino
+++ sketch.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-void refresh(LiquidCrystal_I2C lcd, int startS, int startL, int count)
+void refresh(LiquidCrystal_I2C lcd, unsigned int startS, unsigned int startL, unsigned int count)
 {
   lcd.setCursor(startS, startL);
   for (int i=0; i<count; i++)
@@ -14,32 +14,46 @@ void setup() {
   lcd.init();
   lcd.backlight();
   Serial.begin(9600);
-  lcd.print("R: P1:  P2:")
+  //lcd.print("R:  1:  2:");
 }
-int readSerial(char stopper)
+void serialEvent()
 {
+  unsigned int port=readSerial();
+  unsigned int val=readSerial();
+  lcd.clear();
+  lcd.print(port);
+  lcd.print(" ");
+  lcd.print(val);
+}
+int readSerial()
+{
+  char temp='l';
   int val=0;
-  char temp='j';
-  while (1)
-  {
-    if (Serial.available()>0)
-    {        
-        temp=char( Serial.read() ) ;
-        if(temp!=stopper)
-        {
-          if (temp>='0' && temp<='9')
-          {
-            val=val*10+int(temp-'0');
-            delay(2);
-          }          
-        }          
-        else 
-          return (val);                
-    }
-  }
+  
+        if (Serial.available()>0)
+        {        
+            temp=char( Serial.read() ) ;
+            val=int(temp);
+            /*if(temp!=stopper)
+            {
+              if (temp>='0' && temp<='9')
+              {
+                val=val*10+int(temp-'0');
+                //delay(2);
+              }      
+              val=int(temp);    
+            }          
+            else
+              return (val);*/
+         }
+                              
+       
+       return (val);
+    
+  
+  
+  
 }
 void loop() {
-    t=readSerial('$');
-    refresh(lcd, 0, 0, 3);
-    lcd.print(t);
+    
 }
