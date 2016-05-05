5bb7fa2425cca34b9d8d805e861a3362901067ab
Andrey K <andreykochnov@yandex.ru>
2016-05-05 20:56:57 +0300

    Чтение перекинуто в функцию

--- sketch.ino
+++ sketch.ino
@@ -1,55 +1,45 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include <String.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-int sender=0;
-int val=0;
-char temp='a';
+void refresh(LiquidCrystal_I2C lcd, int startS, int startL, int count)
+{
+  lcd.setCursor(startS, startL);
+  for (int i=0; i<count; i++)
+    lcd.print(" ");
+  lcd.setCursor(startS, startL);
+}
 int t=0;
+const char stoppers[2]={'%', '$'};
 void setup() {
   lcd.init();
   lcd.backlight();
   Serial.begin(9600);
+  lcd.print("R: P1:  P2:")
 }
-
+int readSerial(char stopper)
+{
+  int val=0;
+  char temp='j';
+  while (1)
+  {
+    if (Serial.available()>0)
+    {        
+        temp=char( Serial.read() ) ;
+        if(temp!=stopper)
+        {
+          if (temp>='0' && temp<='9')
+          {
+            val=val*10+int(temp-'0');
+            delay(2);
+          }          
+        }          
+        else 
+          return (val);                
+    }
+  }
+}
 void loop() {
-  
-    while(temp!='%')
-    {
-      while (Serial.available()>0)
-      {        
-        temp=char( Serial.read() ) ;
-        if(temp!='%')
-          sender=sender*10+int(temp-'0');
-        else break;
-        delay(2);        
-      }
-    }
-    lcd.clear();
-    lcd.print("sender done");
-
-    while(temp!='$')
-    {
-      while (Serial.available()>0)
-      {        
-        temp=char( Serial.read() ) ;
-        if(temp!='$')
-          val=val*10+int(temp-'0');
-        else break;
-        delay(2);        
-      }
-    }
-    
-    lcd.clear();
-    lcd.print(sender);
-    lcd.print(" ");
-    lcd.print(val);
-    sender=0;
-    val=0;
-    temp='a';
-
-  /*lcd.print(char('a'+'a'));
-  delay(1000);*/
- 
-
+    t=readSerial('$');
+    refresh(lcd, 0, 0, 3);
+    lcd.print(t);
 }
