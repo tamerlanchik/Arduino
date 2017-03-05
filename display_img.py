56cfe64f218cdcc7915a19af91a4ac01d6bcc6a0
Andrey K <andreykochnov@yandex.ru>
2017-03-05 16:54:20 +0300

    Update display_img.py

--- display_img.py
+++ display_img.py
@@ -4,7 +4,7 @@ from PyQt5.QtGui import QPainter, QBrush, QPen, QPalette
 from dictionary import T
 class DisplayImg(QWidget):
     
-    def __init__(self, size, numb):
+    def __init__(self, size, numb,  Dest,  Rs):
         super().__init__()
         
         #-----РЎР»СѓР¶РµР±РЅР°СЏ РёРЅС„РѕСЂРјР°С†РёСЏ---------
@@ -18,19 +18,21 @@ class DisplayImg(QWidget):
         self.setAutoFillBackground(True)
         self.setPalette(Pal)
         self.numb=numb
+        self.err = False
         
         self.doesShowWayFlag=False
         self.doesShowCirclesFlag=True
         self.doesShowLegsFlag=False
         self.K=21
-        self.crossP=[0, 0]
-        self.legLenghts=[0]*3
-        self.Coords=[0]*3
-        self.test=[0, 0]
+        self.crossP=[0, 0,  0]
+        self.legLenghts= Rs
+        self.Dest=Dest
         if self.numb==0:
-            #self.coordCenter=(self.width()//2, self.height()-self.K)
-            self.coordCenter=(150, 150)
+            self.Mask = (1,  1,  0)
+            self.coordCenter=(self.width()//2, self.height()-self.K)
+            #self.coordCenter=(150, 150)
         elif self.numb==1:
+            self.Mask = (1,  0,  1)
             #self.coordCenter=(self.width()-self.K, self.height()//2)
             self.coordCenter=(100, 230)
         try:  
@@ -45,12 +47,15 @@ class DisplayImg(QWidget):
         
         self.doesShowWayCheck=QCheckBox(T.IS_SHOW_WAY_DRAW_WIDGET, self)
         self.doesShowWayCheck.stateChanged[int].connect(self.doesDrawWays)
+        self.doesShowWayCheck.setCheckState(self.doesShowWayFlag)
         
         self.isShowCircles=QCheckBox(T.IS_SHOW_CIRCLES_DRAW_WIDGET, self)
         self.isShowCircles.stateChanged[int].connect(self.doesDrawCircles)
+        self.isShowCircles.setCheckState(self.doesShowCirclesFlag)
         
         self.isShowLegs=QCheckBox(T.IS_SHOW_LEGS_DRAW_WIDGET, self)
         self.isShowLegs.stateChanged[int].connect(self.doesDrawLegs)
+        self.isShowLegs.setCheckState(self.doesShowLegsFlag)
         
         self.clearButton=QPushButton(T.IS_CLEAR_BUTTON_DRAW_WIDGET, self)
         self.clearButton.clicked.connect(self.clearDisplay)
@@ -68,10 +73,10 @@ class DisplayImg(QWidget):
         self.painter.begin(self)
         #self.painter.translate(QPoint(*self.coordCenter)) 
         self.drawGrid(self.painter)
+        self.showCircles(self.painter)
+        self.showLegs(self.painter)
         self.painter.end()
     
-    def drawTest(self, painter):
-        painter.drawLine(0, 0, 100, 100)
     def drawGrid(self, painter):
         painter.setPen(Qt.gray)
         for i in range(0, self.width()+2, 10):
@@ -80,42 +85,41 @@ class DisplayImg(QWidget):
             painter.drawLine(0, i, self.width(), i)
             
         painter.setPen(Qt.black)
-        if self.numb==0:
-            painter.drawLine(self.width()//2, 0, self.width()//2, self.height())
-            
-            painter.drawLine(0, self.height()-20, self.width(), self.height()-20)
-        elif self.numb==1:
-            painter.drawLine(self.width()-21, 0, self.width()-21, self.height())
-            painter.drawLine(0, self.height()//2, self.width(), self.height()//2)
-            
-        if self.doesShowCirclesFlag==True:
-            self.showCircles(painter)
-        self.showLegs(painter)
+
+        painter.drawLine(self.coordCenter[0], 0, self.coordCenter[0], self.height())
+        painter.drawLine(0, self.coordCenter[1], self.width(), self.coordCenter[1])
     
     def hello(self, data):
         self.legLenghts=data[0]
-        self.Coords=data[1]
+        self.Dest=data[1]
         self.update()
     def showCircles(self, painter):
-        painter.setPen(Qt.red)
-        self.painter.translate(QPoint(*self.coordCenter))
-        painter.setBrush(Qt.red)
-        painter.drawEllipse(QPoint(self.Coords[0], self.Coords[1]), 2, 2) 
-        painter.setBrush(Qt.NoBrush)
-        painter.drawEllipse(QPoint(self.Coords[0], self.Coords[1]), self.legLenghts[1], self.legLenghts[1])
-        
-        painter.setBrush(Qt.red)
-        painter.drawEllipse(QPoint(0, 0), 2, 2) 
-        painter.setBrush(Qt.NoBrush)
-        painter.drawEllipse(QPoint(0, 0), self.legLenghts[0], self.legLenghts[0]) 
-        
-        painter.setBrush(Qt.green)
-        painter.drawEllipse(QPoint(self.crossP[0], self.crossP[1]), 2, 2)
-        
-        painter.setBrush(Qt.blue)
-        painter.drawEllipse(QPoint(self.test[0], self.test[1]), 3, 3)         
+        if self.doesShowCirclesFlag == True:
+            self.painter.translate(QPoint(*self.coordCenter))
+            
+            painter.setBrush(Qt.red)
+            painter.setPen(Qt.red)
+            painter.drawEllipse(QPoint(self.Dest[0], self.Dest[self.numb+1]), 2, 2)
+            painter.drawEllipse(QPoint(0, 0), 2, 2)
+            
+            if self.err == False:
+                painter.setPen(Qt.red)
+            else:
+                painter.setPen(Qt.blue) 
+            painter.setBrush(Qt.NoBrush)
+            painter.drawEllipse(QPoint(0, 0), self.legLenghts[0], self.legLenghts[0])
+            
+            painter.setBrush(Qt.NoBrush)
+            painter.drawEllipse(QPoint(self.Dest[0], self.Dest[self.numb+1]), self.legLenghts[1], self.legLenghts[1])            
+            
+                
     def showLegs(self, painter):
-        painter.drawLine(0, 0, self.crossP[0], self.crossP[1])
+        if self.doesShowLegsFlag == True:
+            if self.numb == 0:
+                painter.drawLine(0, 0, self.crossP[0], self.crossP[self.numb+1])
+                painter.drawLine(self.crossP[0],  self.crossP[self.numb+1],  self.Dest[0],  self.Dest[self.numb+1])
+            else:
+                painter.drawLine(0,  0,  self.Dest[0],  self.Dest[2])
     #----SLOTS----------------------
     def doesDrawCircles(self, flag):
         self.doesShowCirclesFlag=bool(flag)
@@ -129,9 +133,10 @@ class DisplayImg(QWidget):
     def clearDisplay(self):
         self.doesShowWayFlag=False
         self.update()
-    def redrawArea(self, coords, point, pointA):
-        #print("Taken: ", coords, point)
-        self.Coords=coords
-        self.crossP=pointA
-        self.test=point
+    def redrawArea(self, cA = 'a',  cB = 'a',  err = False):
+        if cA != 'a':
+            self.crossP=cA
+        if cB != 'a':
+            self.Dest=cB
+        self.err = err
         self.update()
